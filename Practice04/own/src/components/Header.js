be59be291c4dbd57dc2c9d6fc9390bc6945117e8
aabb15768 <b03204032@ntu.edu.tw>
2019-03-29 20:49:07 +0800

    rm own

--- Practice04/own/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({headerText}) => {
-    return(
-        <header className="todo-app__header">
-            <h1 className="todo-app__title">{headerText}</h1>
-        </header>
-    );
-};
--- Practice04/own/src/components/InputSection.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default ({onKeyPress}) => {
-    return(
-        <section className="todo-app__main">
-            <input placeholder="What needs to be done?" className="todo-app__input" id="todo-input" onKeyPress={onKeyPress}/>
-        </section>
-    );
-};
